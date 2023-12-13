b47dd8115350134fda9857dfaf78a9148087a58a
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-12-13 21:28:12 +0530

    change file location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ import './globals.css'
 // import { v4 as uuidv4 } from 'uuid';
 
 // import { createClient } from "@vercel/kv";
-import Hpage from '../../Hpage'
+import Hpage from '../components/Hpage'
 
 export interface DataTypeDesc {
   dataType: MessageTypeDesc
